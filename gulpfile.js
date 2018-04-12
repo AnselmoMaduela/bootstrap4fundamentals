a3f1306c2fcc9523250315f5aab45079d87b6602
AnselmoMaduela <anselmomaduela@gmail.com>
2018-04-12 08:05:17 +0200

    gulp task to copy scss fles from bootstrap folder in node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require("gulp");
 
+/*
+* #sass task
+* 
+* This task target the bootstrap folder in node_modules,
+* and it copies all scss files to an src folder so that
+* you can change any bootstrap property and by working
+* whith the sass css processor
+* 
+*/
 gulp.task('sass', function() {
-    gulp.src('node_modules/bootstrap/scss')
-    .pipe(gulp.dest('src/'));
+    gulp.src('node_modules/bootstrap/scss/**/**')
+    .pipe(gulp.dest('src/scss/bts'));
 });
